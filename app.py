770c2f6eacbe7af6703f38da14c3084cb6d82c8a
zakmoCA <13905@coderacademy.edu.au>
2023-06-01 19:35:10 +1000

    beginning todoapp backend, simple ui setup

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://admin:spameggs123@localhost:5432/flask_todo_app'
+app.config
 
 db = SQLAlchemy(app)
 
@@ -18,6 +19,25 @@ class Todo(db.Model):
 def create_db():
     db.create_all()
     print('Tables created successfully')  
+
+@app.route('/todos', methods=['POST'])
+def create_todo():
+    # I need to somehow set Todo.title to the user input
+    # Everything else is taken care of by the model
+    # I need to link the creation of this todo (the POST api call) to the submit button/enter key
+    # I need to render this todo to the page
+
+@app.route('/todos', methods=['GET'])
+def get_todo():
+    pass
+
+@app.route('/todos', methods=['PUT'])
+def update_todo():
+    pass
+
+@app.route('/todos', methods=['DELETE'])
+def delete_todo():
+    pass
 
 @app.route('/')
 def index():
