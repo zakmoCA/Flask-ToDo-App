9d2a072ca00e5c3e321fbcab05ffc76ddf164f2d
zakmoCA <13905@coderacademy.edu.au>
2023-06-01 22:04:31 +1000

    todo item pushed to db and displayed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import date
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://admin:spameggs123@localhost:5432/flask_todo_app'
-app.config
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -17,15 +17,25 @@ class Todo(db.Model):
 
 @app.cli.command('create')
 def create_db():
+    db.drop_all()
     db.create_all()
     print('Tables created successfully')  
 
+
+
 @app.route('/todos', methods=['POST'])
 def create_todo():
+    
     # I need to somehow set Todo.title to the user input
     # Everything else is taken care of by the model
     # I need to link the creation of this todo (the POST api call) to the submit button/enter key
     # I need to render this todo to the page
+    title = request.form.get('title')
+    new_todo = Todo(title=title, date_created=date.today())
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 @app.route('/todos', methods=['GET'])
 def get_todo():
@@ -41,7 +51,11 @@ def delete_todo():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # display Todos
+    todo_list = Todo.query.all()
+    return render_template('index.html', todo_list=todo_list)
+
 
 if __name__ == "__main__":
+
     app.run(debug=True)
