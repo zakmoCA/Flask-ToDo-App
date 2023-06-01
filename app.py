461621652c17a8b0c1fcfdc2be1c5976c64b922b
zakmoCA <13905@coderacademy.edu.au>
2023-06-01 10:06:20 +1000

    create command and todo model

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://admin:spameggs123@localhost:5432/flask_todo_app'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    date_created = db.Column(db.Date())
+    is_completed = db.Column(db.Boolean, default=False)
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables created successfully')  
 
 @app.route('/')
 def index():
